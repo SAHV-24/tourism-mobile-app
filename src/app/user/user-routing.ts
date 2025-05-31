80bf51719f2e86d2d6b78cc09ad2b2da4cf8e499
Sergio Herrera <sergioandresherreravallejo@gmail.com>
2025-05-31 11:57:54 -0500

    Merge Done

--- src/app/user/user-routing.ts
+++ src/app/user/user-routing.ts
@@ -1,13 +1,8 @@
 import {Routes} from "@angular/router";
 import {RoleGuard} from "../services/role.guard";
-import {SitesComponent} from "./sites/sites.component";
 import {SitesListComponent} from "./sites/sites-list.component";
 import {FavoritesComponent} from "./favorites/favorites.component";
 import {RoutesComponent} from "./routes/routes.component";
-import {TagListComponent} from "./tags/tag-list.component";
-import {MyTagsComponent} from "./tags/my-tags.component";
-import {TagPageComponent as TagCreateComponent} from "./tags/tag-page/tag-page.component";
-import { AuthGuard } from "../services";
 import { RouteMapComponent } from './route-map/route-map.component';
 
 export const USER_ROUTES: Routes = [
